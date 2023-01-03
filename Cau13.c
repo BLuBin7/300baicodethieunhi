fa125fda3424781d3eea412aa2ae65a26c5f18d5
BLuBin7 <builuongbinh7@gmail.com>
2023-01-03 20:38:54 +0700

    add

---
+++ Cau13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//Bài 13. Số chia hết lớn nhất
+int main()
+{
+    int n,a,b;
+    printf("nhap a : "); // a = 12
+    scanf("%d",&a);
+    printf("nhap b : "); // b=3
+    scanf("%d",&b);
+    if(a%b==0)
+    {
+        printf("vay so chia het lon nhat la : %d",a);
+    }
+    else
+    {
+        int c = a%b;
+        printf("vay so chia het lon nhat la : %d",a-c);
+    }
+}
