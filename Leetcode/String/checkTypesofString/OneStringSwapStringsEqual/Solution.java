fc65b5ecbc8f562fd8434868fbb43d2ac77b4802
BLuBin7 <builuongbinh7@gmail.com>
2023-08-21 23:11:48 +0700

    leetcode

--- Leetcode/String/checkTypesofString/OneStringSwapStringsEqual/Solution.java
+++ Leetcode/String/checkTypesofString/OneStringSwapStringsEqual/Solution.java
@@ -1,74 +1,37 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 //1790. Check if One String Swap Can Make Strings Equal
-public class Solution { public boolean areAlmostEqual(String s1, String s2) {
-        Map<Character, Character> map = new HashMap<>();
-        Set<Character> value = new HashSet();
-        int countkey = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            // neu co key trung
-            if (map.containsKey(s1.charAt(i))) {
-                // neu key trung sau co gtri khac key trung truoc 
-                // tao mot bien dem key, neu a=b va b=a thi dung
-                if (map.get(s1.charAt(i)) != s2.charAt(i)) {
-                    {
-                        if(map.keySet().contains(s2.charAt(i)) == false){
-                           return false;
-                        }
-                        countkey++;
-                    }
-                }
-            } else {
-                map.put(s1.charAt(i), s2.charAt(i));
-                // System.out.println(map);
-            }
-        }
-        
-        int count = 0;
 
+public class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        // convert ve char
+       char[] a = s2.toCharArray() ;
+       // sap xep cac mang char
+    //    Arrays.sort(a);
 
-        
-        
-        System.out.println(map.keySet());
-        for (Character key : map.keySet()) {
-            // lay ra cac key duoi dang Set(ko dc trung cac key)
-            // System.out.println(map.keySet());
-            // System.out.println(map.get(key));
-            // neu khac thi count++
-            // gia tri != key
-            if (map.get(key) != key ) {
-                System.out.println(map.get(key));
-                count++;
+       if(s1.equals(s2)) return true;
+        for (int i = 0; i <s2.length(); i++) {
+           for(int j = i+1;j< s2.length();j++){
+            // swap de kiem tra co trung khong
+               char c = a[i];
+               a[i] = a[j];
+               a[j] = c;
+                                //    mang cac char
+               String temp = new String(a);
+               if(s1.equals(temp)){
+                   return true;
+               }    
+               // neu khong trung thi swap lai nhu ban dau
+               a[j] = a[i];
+               a[i] = c;
             }
             
-            
-            // if(map.keySet().size() < map.values().size()){
-            // return false;
-            // }
-
-            // if(map.containsKey(key)&&map.containsKey(value)){
-            // count++;
-            // }
         }
-        if(s1.equals(s2)){
-            return true;
-        }
-        return countkey % 2 == 0 &&countkey>0||count<=2&&count>0;
+        return false;
     }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.areAlmostEqual(
-                "siyolsdcjthwsiplccjbuceoxmpjgrauocx",
-                "siyolsdcjthwsiplccpbuceoxmjjgrauocx"));
-        System.out.println(solution.areAlmostEqual(
-                "aa",
-                "ab"));
-        System.out.println(solution.areAlmostEqual(
-        "bank3","kanb3"
-        ));
+        System.out.println(solution.areAlmostEqual("bank", "kanb"));
+        String s1= "bank";
+        char[] ab = s1.toCharArray();
+        System.out.println(ab);
     }
 }
