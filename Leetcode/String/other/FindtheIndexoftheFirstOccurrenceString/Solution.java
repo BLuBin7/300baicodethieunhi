d78eb07ba22e332a95504739cf71f862619c4d96
BLuBin7 <builuongbinh7@gmail.com>
2023-08-27 22:40:25 +0700

    Problems Solved

---
+++ Leetcode/String/other/FindtheIndexoftheFirstOccurrenceString/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.equals(haystack)) return 0;
+        for (int i = 0; i < haystack.length() ; i++) {
+            if (i <= haystack.length() - needle.length()) {
+                if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                    return i;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.strStr("abc", "c"));
+    }
+}
