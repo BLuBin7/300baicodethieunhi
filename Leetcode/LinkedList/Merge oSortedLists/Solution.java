ed18daa300503d3664b0a52c62f448250c11da78
BLuBin7 <builuongbinh7@gmail.com>
2023-08-30 21:35:03 +0700

    Problems Solved

---
+++ Leetcode/LinkedList/Merge oSortedLists/Solution.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+        if (list1 == null)
+            return list2;
+        return list1;
+    }
+    void display(ListNode head){
+        ListNode temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+        solution.display(list1);
+        
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+        solution.display(list2);
+
+        // Merge the two lists
+        ListNode merged = solution.mergeTwoLists(list1, list2);
+
+        // Print the merged list
+        System.out.print("Merged List: ");
+        while (merged != null) {
+            System.out.print(merged.val + " ");
+            merged = merged.next;
+        }
+    }
+}
