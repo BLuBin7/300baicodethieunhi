b77c871c24024d6a9bd202fcea979e15c0382a70
BLuBin7 <builuongbinh7@gmail.com>
2023-09-07 22:19:18 +0700

    Problems Solved

--- Leetcode/LinkedList/ConvertBinaryNumber/Solution.java
+++ Leetcode/LinkedList/ConvertBinaryNumber/Solution.java
@@ -1,8 +1,24 @@
+import java.util.Stack;
+
 public class Solution {
     public int getDecimalValue(ListNode head) {
-        
+        ListNode node = head;
+        String result="";
+        while (node != null) {
+            result +=node.val;
+            node = node.next;
+        }
+        // chuyen sang co so 2
+        int x = Integer.parseInt(result, 2);
+        return x;
     }
+
+
     public static void main(String[] args) {
-        
+        Solution solution = new Solution();
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(0);
+        list1.next.next = new ListNode(1);
+        System.out.println(solution.getDecimalValue(list1));
     }
 }
