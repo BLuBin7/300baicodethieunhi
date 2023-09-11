e9cbb968704ac91981662416531266b2364ebf0d
BLuBin7 <builuongbinh7@gmail.com>
2023-09-11 23:44:34 +0700

    Problems Solved

--- Leetcode/BinarySearch/MissingNumber/Solution.java
+++ Leetcode/BinarySearch/MissingNumber/Solution.java
@@ -14,27 +14,41 @@ public class Solution {
         return nums;
     }
 
+    // public int missingNumber(int[] nums) {
+    //     sort(nums);
+    //     int max = nums[nums.length - 1];
+    //     int min = nums[0];
+    //     int n = max - min;
+    //     int avg = 0;
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (i != 0 && i != nums.length - 1 && nums[i] > nums[i - 1] &&
+    //                 nums[i] < nums[i + 1]) {
+    //             avg = (nums[i] + nums[i + 1]) / 2;
+    //             System.out.println(avg);
+    //         }
+    //         map.put(i, nums[i]);
+    //     }
+    //     if (!map.containsValue(avg)) {
+    //         return avg;
+    //     }
+    //     return n;
+    // }
+    // Solution 2
     public int missingNumber(int[] nums) {
-        sort(nums);
-        int max = nums[nums.length - 1];
-        int min = nums[0];
-        int n = max - min;
-        int avg = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (i != 0 && i != nums.length - 1 && nums[i] > nums[i - 1] &&
-                    nums[i] < nums[i + 1]) {
-                avg = (nums[i] + nums[i + 1]) / 2;
-                System.out.println(avg);
-            }
-            map.put(i, nums[i]);
-        }
-        if (!map.containsValue(avg)) {
-            return avg;
-        }
-        return n;
-    }
-
+        int n = nums.length;
+      int sum = 0;
+      while(n>0){
+          sum +=n;
+          n--;
+      }
+      int arrSum = 0;
+      for (int i = 0;i<nums.length;i++){
+          arrSum +=nums[i];
+      }
+      
+      return (sum-arrSum);
+  }
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.missingNumber(new int[] { 1, 2, 4, 5 }));
