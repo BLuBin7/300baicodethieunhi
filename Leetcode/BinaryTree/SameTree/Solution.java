37670b4486a00b992f3669b8ac62e8ef47edc31b
BLuBin7 <builuongbinh7@gmail.com>
2023-09-30 22:11:31 +0700

    Problems Solved

---
+++ Leetcode/BinaryTree/SameTree/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Base case: if both trees are null, they are identical
+        if (p == null && q == null) {
+            return true;
+        }
+        // If only one tree is null or the values are different, they are not identical
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        // Recursively check if the left and right subtrees are identical
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
