f91ccb985cdc97ea96aadfc4dd7763a1b7aa1a48
BLuBin7 <builuongbinh7@gmail.com>
2023-10-03 00:33:51 +0700

    Problems Solved

--- Leetcode/BinaryTree/SymmetricTree/Solution.java
+++ Leetcode/BinaryTree/SymmetricTree/Solution.java
@@ -12,9 +12,18 @@ public class Solution {
         if (node1 == null || node2 == null) {
             return false;
         }
+        System.out.println("node1: " + node1.val + " node2: " + node2.val);
         return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
     }
     public static void main(String[] args) {
-        
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        System.out.println(solution.isSymmetric(root));
     }
 }
