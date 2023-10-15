89df90bf7e220a4a1922607b9e43b05ceca5c958
BLuBin7 <builuongbinh7@gmail.com>
2023-10-15 22:33:59 +0700

    Problems Solved

---
+++ Leetcode/BinaryTree/SubtreeofAnotherTree/Solution.java
@@ -0,0 +1,33 @@
+public class Solution{
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        TreeNode subRoot = new TreeNode(4);
+        subRoot.left = new TreeNode(1);
+        subRoot.right = new TreeNode(2);
+        System.out.println(s.isSubtree(root, subRoot));
+    }
+}
