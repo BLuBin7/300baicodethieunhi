592dab31b6ecb34ebc6b23b1417070bfb0c63be6
BLuBin7 <builuongbinh7@gmail.com>
2023-10-20 00:58:48 +0700

    update

--- Leetcode/BinaryTree/DiameterofBinaryTree/Solution.java
+++ Leetcode/BinaryTree/DiameterofBinaryTree/Solution.java
@@ -1,21 +1,28 @@
 public class Solution {
-    int HighLeft = 0;
-    int HighRight = 0;
+    int HeightLeft = 0;
+    int HeightRight = 0;
     int answer = 0;
-    public int diameterOfBinaryTree(TreeNode root) {
+    public int dfs(TreeNode root) {
         if(root == null){
             return -1;
         }
-        else if(root.left == null&& root.right == null){
+        if(root.left == null && root.right == null){
             return 0;
         }
-        HighLeft = diameterOfBinaryTree(root.left);
-        HighRight = diameterOfBinaryTree(root.right);
-        answer = Math.max(answer, HighLeft + HighRight + 2);
+        HeightLeft = dfs(root.left);
+        HeightRight = dfs(root.right);
+        answer = Math.max(answer, HeightLeft + HeightRight + 2);
 
-        return Math.max(HighLeft, HighRight) + 1;
+        return Math.max(HeightLeft, HeightRight) + 1;
     }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return answer;
+    }
+
     public static void main(String[] args) {
-        
+        Solution solution = new Solution();
     }
+    
 }
