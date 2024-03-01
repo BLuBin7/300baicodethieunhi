b1f245e8eeba84d752c999510d829693c8631306
BLuBin7 <builuongbinh7@gmail.com>
2024-03-01 23:10:06 +0700

    2864. Maximum Odd Binary Number

--- Leetcode/DailyChallenge-2024-LeetCode/03_March_2024/1_Maximum Odd Binary Number/Solution.java
+++ Leetcode/DailyChallenge-2024-LeetCode/03_March_2024/1_Maximum Odd Binary Number/Solution.java
@@ -1,3 +1,27 @@
-public class Solution {
-    
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int num1 = 0;
+        StringBuilder strBuilder = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                num1++;
+            }
+        }
+
+        num1--;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (num1 > 0) {
+                strBuilder.append('1');
+                num1--;
+            } else {
+                strBuilder.append('0');
+            }
+        }
+
+        strBuilder.append('1');
+
+        return strBuilder.toString();
+    }
 }
