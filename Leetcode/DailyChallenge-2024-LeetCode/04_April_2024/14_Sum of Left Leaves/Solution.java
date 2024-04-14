03ad665d7349cfa0c79089a1c21f8794d7a7cb4a
BLuBin7 <builuongbinh7@gmail.com>
2024-04-15 01:43:34 +0700

    Sum of Left Leaves

---
+++ Leetcode/DailyChallenge-2024-LeetCode/04_April_2024/14_Sum of Left Leaves/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+       if(root == null)
+           return 0;
+       if(isLeafNode(root.left)){
+           return root.left.val + sumOfLeftLeaves(root.right);
+       }
+       return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+   }
+
+   private boolean isLeafNode(TreeNode root){
+       if(root == null)
+           return false;
+       return root.left == null && root.right == null;
+   }
+}
