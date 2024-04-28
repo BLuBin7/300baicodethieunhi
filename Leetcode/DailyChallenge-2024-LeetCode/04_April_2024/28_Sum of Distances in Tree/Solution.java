6923979de1d4c7190bc9ce393998317107261b7a
BLuBin7 <builuongbinh7@gmail.com>
2024-04-29 01:32:27 +0700

    Sum of Distances in Tree

---
+++ Leetcode/DailyChallenge-2024-LeetCode/04_April_2024/28_Sum of Distances in Tree/Solution.java
@@ -0,0 +1,47 @@
+class Solution {
+    private Map<Integer, List<Integer>> graph;
+    private int[] count;
+    private int[] res;
+
+    private void dfs(int node, int parent) {
+        for (int child : graph.get(node)) {
+            if (child != parent) {
+                dfs(child, node);
+                count[node] += count[child];
+                res[node] += res[child] + count[child];
+            }
+        }
+    }
+
+    private void dfs2(int node, int parent) {
+        for (int child : graph.get(node)) {
+            if (child != parent) {
+                res[child] = res[node] - count[child] + (count.length - count[child]);
+                dfs2(child, node);
+            }
+        }
+    }
+
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        graph = new HashMap<>();
+        count = new int[n];
+        res = new int[n];
+        Arrays.fill(count, 1);
+        
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        dfs(0, -1);
+        dfs2(0, -1);
+
+        return res;
+    }
+}
